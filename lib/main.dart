2ad63edf91ff924c144a1fcb98861ae26e1d92e7
Nandhitha2001 <nandhitha0987@gmail.com>
2023-05-16 15:03:17 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(Center(
+      child: Text(
+        'grwpc',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(color:Colors.pink,
+        fontSize: 35,
+        fontWeight: FontWeight.bold,
+        fontStyle: FontStyle.italic),
+      )));
+}
